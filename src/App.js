eb3a827e489442b34b9489197507ca1836b62be4
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-10-07 19:43:22 -0500

    Add all project data in data/index.js -- waiting to add images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
-
+import data from './data'
 import './App.css';
 
 function App() {
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
-        <nav>
-          <ul>
-            <li>Home</li>
+        <nav className="App-navbar">
+          <a href="/" className="App-link" >JS for Designers</a>
+          <ul className="App-nav">
+            <li>About</li>
+            <li>Projects</li>
+            
           </ul>
         </nav>
+        
       </header>
+      <main className="App-projects-list">
+
+        
+          <section className="App-project-card">
+            <div className="App-project-card--text">
+              <h2>Plant Life</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quod facilis voluptates vitae exercitationem esse nobis. Aut quaerat distinctio ipsum?</p>
+            </div>
+            <figure className="App-project-card--item">
+              {/* <img className="App-project-card--image" src={} alt="Plant Life Project Screenshot"/> */}
+            </figure>
+          </section>
+         
+        </main>
     </div>
   );
 }
---
+++ src/data/index.js
@@ -0,0 +1,32 @@
+exports.data = [
+    {
+        title: 'Plant Life',
+        description: 'We go further with Javascript events by adding in click, mouseover and mouseout events, plus talking about how to add randomness into our design work.',
+        image: './assets/plant-life/assets/plant-life.png',
+        link: '',
+    },
+    {
+        title: 'Type Checker',
+        description: 'With Type Checker, we discuss how to work with form tags such as inputs, sliders, checkboxes and select boxes to build up interactivity to our projects.',
+        image: './assets/type-checker/assets/type-checker.png',
+        link: '',
+    },
+    {
+        title: 'Jenna Buchholz',
+        description: 'With this project, we go in depth into scroll events and parallax by adding progress bars, waypoints and thresholds.',
+        image: './assets/jenna-buchholz/assets/jenna-buchholz.png',
+        link: '',
+    },
+    {
+        title: 'Brux',
+        description: 'We extend our Javascript events knowledge by talking more about scroll and mouse movement events to add design elements such as fade on scroll and eyes that follow your cursor.',
+        image: './assets/brux/assets/brux.png',
+        link: '',
+    },
+    {
+        title: 'Words of Womxn',
+        description: 'We tie all of our learnings together by adding layers of Javascript to a project to take it from something static into a dynamic, fun site.',
+        image: './assets/type-checker/assets/type-checker.png',
+        link: '',
+    },
+]

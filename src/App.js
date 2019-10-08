968a69c13edce16cb09b82f6cb9f4fba55fdc5b0
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-10-08 01:52:52 -0500

    Update App.js to include PlantLife route. Add nested routes for PlantLife Page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         
         <Route 
-          exact path="/plant-life" 
+          path="/plant-life" 
           component={PlantLife} 
           /> 
        
--- src/Pages/PlantLife/index.jsx
+++ src/Pages/PlantLife/index.jsx
@@ -5,7 +5,17 @@ import {  Route, Link, Switch} from 'react-router-dom'
 class PlantLife extends Component {
     state = {  }
     render() { 
-        return ( <div>hello</div> );
+        return ( 
+            <div>
+                <Switch>
+                    <Route exact path="/plant-life" render={() => (<div>Home</div>)} />
+                    <Route path="/plant-life/shop" render={() => (<div>Shop</div>)} />
+                    <Route path="/plant-life/about" render={() => (<div>About</div>)} />
+                    <Route path="/plant-life/help" render={() => (<div>Help</div>)} />
+                    <Route path="/plant-life/blog" render={() => (<div>Blog</div>)} />
+                </Switch>
+            </div>
+         );
     }
 }
  

56a5a757af03fa4f1dbeb5b8c42bbe064e03aaa7
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-10-09 06:42:38 -0500

    Add all input tags including color selectors. No functionality with inputs 2 through 7 yet

--- src/Pages/TypeChecker/components/ControlPanel/index.jsx
+++ src/Pages/TypeChecker/components/ControlPanel/index.jsx
@@ -6,6 +6,20 @@ const ControlPanel = ({handleTextChange, sentence}) => {
         <section className="control-panel--wrapper">
             <h1>Type Checker</h1>
             <Control name="sentence" htmlFor="sentence" handleChange={handleTextChange} placeholder="The quick brown fox jumps over the lazy dog"  label="Your Text" type="text" value={sentence}/>
+            <Control name="typeFace" htmlFor="typeFace" label="Typeface" type="select" options={['Option 1', 'Option 2', 'Option 3']}/>
+            <Control name="typeSize" htmlFor="typeSize" label="Type size" type="range" />
+            <Control name="fontWeight" htmlFor="fontWeight" label="Font weight" type="range" />
+            <Control name="leading" htmlFor="leading" label="Leading" type="range" />
+            <Control name="italic" htmlFor="italic" label="Italic" type="checkbox" />
+            <div className="control">
+                <label htmlFor="colors">Colors</label>
+                <div name="colors" className="colors">
+                    <p>a</p>
+                    <p>a</p>
+                    <p>a</p>
+                    <p>a</p>
+                </div>
+            </div>
         </section>
      );
 }
@@ -13,11 +27,20 @@ const ControlPanel = ({handleTextChange, sentence}) => {
 export default ControlPanel;
 
 
-const Control = ({htmlFor, type, name, label, placeholder, handleChange, value}) => {
+const Control = ({htmlFor, type, name, label, placeholder, handleChange, value, options}) => {
     return ( 
         <div className="control">
             <label htmlFor={htmlFor}>{label}</label>
-            <input value={value} onChange={handleChange} placeholder={placeholder}  type={type} name={name}/>
+            {
+                type === 'select' ? 
+                (
+                    <select> 
+                        {options.map(option => <option>{option}</option>)} 
+                    </select>
+                ) 
+                : 
+                ( <input value={value} onChange={handleChange} placeholder={placeholder}  type={type} name={name}/> )
+            }
         </div> 
     );
 }

e1f2946d5812e4c874ce868fdaa0a1485e4b1dfb
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-10-08 19:49:21 -0500

    Export TypeChecker component

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,10 @@
 import HomePage from "./HomePage"
 import PlantLife from './PlantLife'
-
+import TypeChecker from './TypeChecker'
 
 
 export {
     HomePage,
-    PlantLife
+    PlantLife,
+    TypeChecker
 }

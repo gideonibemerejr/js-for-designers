d73465813ae8105de6bd76d7adb07c1d2faafb24
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-10-31 13:28:57 -0500

    Update isDark for slide 3

--- src/Pages/JennaBuchholz/components/Section/index.jsx
+++ src/Pages/JennaBuchholz/components/Section/index.jsx
@@ -4,35 +4,46 @@ import { Square, Circle } from '../Shapes'
 import './Section.css'
 
 
-const Section = React.forwardRef(({ client, imageNo, circle, square, photo, backgroundColor, c }, ref) => (
-    <section ref={ref} data-client={client} data-page={imageNo % 2 === 0 ? '2 / 2' : '1 / 2'} data-is-dark={imageNo === 3 ? true : false} className={`JB-section ${client}${imageNo}`} style={{backgroundColor}} >
-            <div className="JB-content">
-            {
-                   c === 'AlterEgo' && imageNo === 1  ? 
-                   (
-                    <>
-                    <Square backgroundColor={square}/>
-                        <img src={photo} alt=""/>
-                    <Circle backgroundColor={circle} />
-                    </>
-                   ) 
-                   :  
-                   (
-                    <>
-                    <Circle backgroundColor={circle} />
-                        <img src={photo} alt=""/>
-                    <Square backgroundColor={square}/>
-                    </>
-                    
-                   )
+const Section = React.forwardRef(({ client, imageNo, circle, square, photo, backgroundColor, c }, ref) => {
+    const dataDark = imageNo === 3 ? true : false
+    return (
+    <section 
+    ref={ref} 
+    data-client={client} 
+    data-page={imageNo % 2 === 0 ? '2 / 2' : '1 / 2'} 
+    data-is-dark={dataDark} 
+    className={`JB-section ${client}${imageNo}`} 
+    style={{backgroundColor}} 
+    >
+        <div className="JB-content">
+        {
+                c === 'AlterEgo' && imageNo === 1  ? 
+                (
+                <>
+                <Square backgroundColor={square}/>
+                    <img src={photo} alt=""/>
+                <Circle backgroundColor={circle} />
+                </>
+                ) 
+                :  
+                (
+                <>
+                <Circle backgroundColor={circle} />
+                    <img src={photo} alt=""/>
+                <Square backgroundColor={square}/>
+                </>
                 
-                
-                }
-                
-            </div>
-        </section>
+                )
+            
+            
+            }
+            
+        </div>
+    </section>
 
-))
+    )
+
+})
 
 
 export default Section;
